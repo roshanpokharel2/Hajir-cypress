fd17cdfeb373df95e202b1c811ba64358172da46
roshanpokharel2 <techwithroshan123@gmail.com>
2024-03-12 14:52:19 +0545

    created html report generator for scripts

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",
-
-  reporterOptions: {
-    overwrite: false,
-    html: true,
-    json: true,
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
-
-  waitForFileChange: false
 });
